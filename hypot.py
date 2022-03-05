14580b3a598a051186958858c6c30dcb8d58feb4
MISBAH UDDIN <100334054+MISBAHUDDIN786@users.noreply.github.com>
2022-03-05 17:13:45 +0530

    Add files via upload

---
+++ hypot.py
@@ -0,0 +1,4 @@
+perp=int(input("enter the length of perpendicular"))
+base=int(input("enter the length of base"))
+hypot=((((perp)**2)+((base)**2))**(1/2))
+print("the length of hypotenuse is",hypot)
