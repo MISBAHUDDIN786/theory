40d49f63f395b00fa0c8b8f690e4103a6ddcbc31
MISBAH UDDIN <100334054+MISBAHUDDIN786@users.noreply.github.com>
2022-03-05 17:23:25 +0530

    Add files via upload

---
+++ maxof_2num.py
@@ -0,0 +1,6 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+if(a>b):
+    print("first number is greater than second numbner")
+else:
+    print("second number is greater than first number")
